539e1f31f3eab47a7052d304da968f8cfcc963c9
Harsh Sahare <harshsahare1212@gmail.com>
2025-05-11 04:08:56 +0530

    refactored : added animation in the properties section

--- app/components/sections/propertiesSection.tsx
+++ app/components/sections/propertiesSection.tsx
@@ -1,21 +1,43 @@
-// app/properties/PropertiesPage.tsx or pages/properties.tsx
+"use client";
 import React from "react";
 import PropertyCard from "../propertyCard";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 const PropertiesSection = () => {
+  gsap.registerPlugin(ScrollTrigger);
+
+  useGSAP(() => {
+    const ctx = gsap.context(() => {
+      gsap.from(".properties-container .heading", {
+        opacity: 0,
+        x: -50,
+        duration: 1,
+        scrollTrigger: {
+          trigger: ".properties-container",
+          start: "top 0%",
+        },
+        stagger: 0.2,
+      });
+    });
+
+    return () => ctx.revert();
+  }, []);
+
   return (
-    <section className="w-[95.5%] min-h-svh flex flex-col justify-center ml-auto pl-6 mt-[174px] py-12">
+    <section className="w-[95.5%] properties-container min-h-svh flex flex-col justify-center ml-auto pl-6 mt-[174px] py-12">
       <div className="flex flex-col lg:flex-row justify-between  gap-12">
         {/* Text Content */}
         <div className="lg:w-1/3 mt-[115px]">
-          <p className="text-xs font-classica tracking-widest text-gray-500 flex items-center">
+          <p className="text-xs font-classica heading tracking-widest text-gray-500 flex items-center">
             PROPERTIES
-            <span className="flex-grow max-w-[150px] border-t border-gray-300 ml-4"></span>
+            <span className="flex-grow max-w-[150px] heading border-t border-gray-300 ml-4"></span>
           </p>
-          <h2 className="text-4xl font-classica font-[400] mt-4 mb-[40px] leading-snug">
+          <h2 className="text-4xl font-classica heading font-[400] mt-4 mb-[40px] leading-snug">
             Discover Our <br /> Newest Residential Property
           </h2>
-          <p className="text-[#6A5F6C] text-[11px]/[20px] font-poppins mb-[48px]">
+          <p className="text-[#6A5F6C] heading text-[11px]/[20px] font-poppins mb-[48px]">
             Dur Meraas designs communities that respond to <br /> contemporary
             passions for every lifestyle. <br /> Choose from alluring waterfront
             locations, <br />
@@ -29,7 +51,7 @@ const PropertiesSection = () => {
             City
           </p>
           <button
-            className="border border-black text-[#747176] hover:bg-black hover:text-white transition text-[12px]  font-poppins tracker-[1px] rounded-full"
+            className="border border-black heading text-[#747176] hover:bg-black hover:text-white transition text-[12px]  font-poppins tracker-[1px] rounded-full"
             style={{ padding: "17px 53px" }}
           >
             View All Properties
