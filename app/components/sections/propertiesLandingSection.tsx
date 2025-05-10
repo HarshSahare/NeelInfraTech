521a06e6987f9e14084a1946ad0ed0c8c148bebb
Harsh Sahare <harshsahare1212@gmail.com>
2025-05-11 04:02:20 +0530

    refactored : added animation in the properties landing section

--- app/components/sections/propertiesLandingSection.tsx
+++ app/components/sections/propertiesLandingSection.tsx
@@ -1,26 +1,72 @@
+"use client";
 import Image from "next/image";
 import React from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 const PropertiesLandingSection = () => {
+  gsap.registerPlugin(ScrollTrigger);
+
+  useGSAP(() => {
+    const ctx = gsap.context(() => {
+      gsap.from(".properties-landingpage-container .heading", {
+        opacity: 0,
+        x: -50,
+        duration: 1,
+        scrollTrigger: {
+          trigger: ".properties-landingpage-container",
+          start: "top 0%",
+        },
+        stagger: 0.2,
+      });
+    });
+
+    gsap.from(".up-grid", {
+      opacity: 0,
+      y: "-100%",
+      duration: 1,
+      scrollTrigger: {
+        trigger: ".properties-landingpage-container",
+        start: "top 0%",
+        pin: true,
+        scrub: 2,
+      },
+    });
+
+    gsap.from(".down-grid", {
+      opacity: 0,
+      y: "100%",
+      duration: 1,
+      scrollTrigger: {
+        trigger: ".properties-landingpage-container",
+        start: "top 0%",
+        scrub: 2,
+      },
+    });
+
+    return () => ctx.revert();
+  }, []);
+
   return (
-    <section className="flex flex-col lg:flex-row gap-[75px] max-w-[93.5%] mx-auto">
+    <section className="flex properties-landingpage-container flex-col lg:flex-row gap-[75px] max-w-[93.5%] mx-auto">
       {/* Left Column: Text Content */}
       <div className="lg:w-1/3  mt-[151px] lg:mb-0">
-        <p className="text-[10px] font-classica tracking-widest text-gray-500 flex items-center">
+        <p className="text-[10px] font-classica heading tracking-widest text-gray-500 flex items-center">
           PROPERTIES
-          <span className="flex-grow border-t max-w-[150px] border-gray-300 ml-4"></span>
+          <span className="flex-grow border-t max-w-[150px] heading border-gray-300 ml-4"></span>
         </p>
-        <h2 className="text-[40px] font-classica font-[400] mt-4 mb-[40px] leading-snug">
+        <h2 className="text-[40px] font-classica font-[400] heading mt-4 mb-[40px] leading-snug">
           PREMIUM <br /> PROPERTIES IN THE <br /> BEST LOCATIONS
         </h2>
-        <p className="text-[#6A5F6C] text-[13px]/[22px] font-poppins mb-[48px]">
+        <p className="text-[#6A5F6C] text-[13px]/[22px] heading font-poppins mb-[48px]">
           Dur properties, located in prime areas, boast unique <br /> designs
           and aspirational lifestyles within vibrant Emaar <br /> communities,
           all seamlessly managed by Emaar <br /> Community Management&apos;s
           dedicated team.
         </p>
         <button
-          className="bg-black  text-white text-[12px]  font-poppins tracker-[1px] rounded-full"
+          className="bg-black  text-white text-[12px] heading font-poppins tracker-[1px] rounded-full"
           style={{ padding: "17px 53px" }}
         >
           View All Properties
@@ -30,7 +76,7 @@ const PropertiesLandingSection = () => {
       {/* Right Column: Image Grid */}
       <div className="lg:w-2/3 flex gap-6" id="property-images">
         {/* Column 1 */}
-        <div className="flex flex-col gap-6 w-1/2">
+        <div className="flex up-grid flex-col gap-6 w-1/2">
           {/* Image 1 */}
           <div>
             <Image
@@ -67,7 +113,7 @@ const PropertiesLandingSection = () => {
         </div>
 
         {/* Column 2 */}
-        <div className="flex flex-col gap-6 w-1/2">
+        <div className="flex down-grid flex-col gap-6 w-1/2">
           {/* Image 2 */}
           <div className="mt-[.5%]">
             <Image
