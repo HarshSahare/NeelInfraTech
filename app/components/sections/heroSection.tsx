d8e26568b5dfaa2f16be526202bda9bf3438c135
Harsh Sahare <harshsahare1212@gmail.com>
2025-05-11 17:23:38 +0530

    refactored : removed the bottom section of the hero section and adjusted animation

--- app/components/sections/heroSection.tsx
+++ app/components/sections/heroSection.tsx
@@ -8,14 +8,15 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 function HeroSection() {
   gsap.registerPlugin(ScrollTrigger);
-  const svg = useRef<SVGPathElement>(null);
+  const svg = useRef<SVGSVGElement>(null);
+  const path = useRef<SVGPathElement>(null);
   const [currentVideo, setCurrentVideo] = useState(0);
 
   useGSAP(() => {
     const ctx = gsap.context(() => {
       const timeline = gsap.timeline({
         scrollTrigger: {
-          trigger: ".hero-section", // Ensure this exists
+          trigger: ".hero-section",
           start: "top top",
           end: "top+=300",
           scrub: 0.3,
@@ -23,7 +24,7 @@ function HeroSection() {
         },
       });
       timeline.to(
-        svg.current,
+        path.current,
         {
           attr: { d: "M 0 100 L 50 0 L 100 100 Z ", fill: "#ffffff50" },
         },
@@ -36,13 +37,22 @@ function HeroSection() {
         },
         "hero-same"
       );
-      timeline.to(svg.current, {
-        scale: 3,
-        attr: {
-          fill: "#fff",
+      timeline.to(
+        path.current,
+        {
+          attr: {
+            fill: "#fff",
+          },
         },
-        transformOrigin: "center center",
-      });
+        "hero-same-2"
+      );
+      timeline.to(
+        svg.current,
+        {
+          scale: 3,
+        },
+        "hero-same-2"
+      );
     });
 
     return () => ctx.revert();
@@ -60,32 +70,13 @@ function HeroSection() {
     <div className="w-full relative hero-section h-screen bg-black overflow-hidden">
       <div className="overlay bg-[#12121277] absolute top-0 left-0 h-full w-full z-2"></div>
       <video
-        className={`absolute top-0 left-0 w-full h-full object-cover transition-opacity duration-1000 z-1 ${
-          currentVideo != 0 && "opacity-0"
-        }`}
+        className={`absolute top-0 left-0 w-full h-full object-cover transition-opacity duration-1000 z-1`}
         autoPlay
         muted
         loop
         src="/videos/defaultHeroVideo.mp4"
       />
-      <video
-        className={`absolute top-0 left-0 w-full h-full object-cover transition-opacity duration-1000 z-1 ${
-          currentVideo != 1 && "opacity-0"
-        }`}
-        autoPlay
-        muted
-        loop
-        src="/videos/commercialHeroVideo.mp4"
-      />
-      <video
-        className={`absolute top-0 left-0 w-full h-full object-cover transition-opacity duration-1000 z-1 ${
-          currentVideo != 2 && "opacity-0"
-        }`}
-        autoPlay
-        muted
-        loop
-        src="/videos/residentialHeroVideo.mp4"
-      />
+
       <div className="overlayTexts relative w-full h-full absolute top-0 left-0 z-3">
         <div className="layer1 absolute top-0 left-0 w-full h-full flex justify-center content-center delay-4s fadeOut  flex-wrap ">
           <div
@@ -95,8 +86,8 @@ function HeroSection() {
           </div>
         </div>
 
-        <div className="layer2 absolute top-0 text-white left-0 w-full h-full opacity-0 flex justify-center content-end flex-wrap delay-5s z-3 fadeIn">
-          <div className="h-[70%] w-full flex items-center flex-col">
+        <div className="layer2 absolute top-0 text-white left-0 w-full h-full opacity-0 flex justify-center content-center flex-wrap delay-5s z-3 fadeIn">
+          <div className="h-fit m-auto content-container w-full flex items-center flex-col">
             <div className="top-section flex flex-col items-center">
               <div className="title font-classica heading uppercase text-[40px] font-[400]">
                 The Art of Fine Living.
@@ -106,85 +97,23 @@ function HeroSection() {
                 your lifestyle and reflect a taste for only the finest.
               </div>
             </div>
-            <div className="bottom-section flex-1 opacity-0 fadeIn delay-6s  mt-[155px] w-full flex max-w-[1000px] ">
-              <div
-                className={`flex flex-1 flex-col sectionTransition mt-auto ${
-                  currentVideo == 1 ? "expandedSection" : "shrunkSection"
-                }`}
-              >
-                <div className="mx-auto">
-                  <div
-                    className="title text-[28px] font-[100] uppercase font-classica cursor-pointer"
-                    onClick={() => toggleSections(1)}
-                  >
-                    Commercial Properties
-                  </div>
-                  <div
-                    className={`description font-poppins text-[14px]/[20px] font-[50] opacity-0  mt-[15px] delay-500 transition-opacity  max-w-[418px] ${
-                      currentVideo == 1 ? "fadeIn" : "fadeOut"
-                    }`}
-                  >
-                    We offer prime commercial spaces ideal for shops, offices,
-                    and showrooms—perfect for business growth and smart
-                    investment.
-                  </div>
-                  <div
-                    className={`button w-fit px-4 py-2 cursor-pointer opacity-0  bg-white rounded mt-5 transition-opacity delay-500 text-black ${
-                      currentVideo == 1 ? "fadeIn" : "fadeOut"
-                    }`}
-                  >
-                    Learn More
-                  </div>
-                </div>
-              </div>
-              <div className="flex w-[.5px] h-[50%] bg-white"></div>
-              <div
-                className={`flex flex-1 flex-col sectionTransition mt-auto ${
-                  currentVideo == 2 ? "expandedSection" : "shrunkSection"
-                }`}
-              >
-                <div className="mx-auto">
-                  <div
-                    className="title text-[28px] font-[100] uppercase font-classica cursor-pointer"
-                    onClick={() => toggleSections(2)}
-                  >
-                    Residential properties
-                  </div>
-                  <div
-                    className={`description font-poppins opacity-0 text-[14px]/[20px] font-[50] mt-[15px] delay-500 transition-opacity  max-w-[418px] ${
-                      currentVideo == 2 ? "fadeIn" : "fadeOut"
-                    }`}
-                  >
-                    We Offer Affordable residential plots and homes in prime
-                    locations—perfect for families looking to build their dream
-                    home in a peaceful, well-connected area
-                  </div>
-                  <div
-                    className={`button w-fit px-4 py-2 opacity-0 cursor-pointer bg-white rounded mt-5 transition-opacity delay-500 text-black ${
-                      currentVideo == 2 ? "fadeIn" : "fadeOut"
-                    }`}
-                  >
-                    Learn More
-                  </div>
-                </div>
-              </div>
-            </div>
           </div>
         </div>
 
         <div className="layer3 absolute top-0 w-full h-full z-20 pointer-events-none"></div>
 
-        <div className="layer4 z-2 absolute bottom-0 left-0 h-full w-full">
+        <div className="layer4 z-2 absolute bottom-0 left-0 h-full w-full overflow-hidden">
           <svg
             viewBox="0 0 100 100"
             preserveAspectRatio="none"
             xmlns="http://www.w3.org/2000/svg"
+            ref={svg}
             className="w-screen h-screen"
           >
             <path
-              ref={svg}
+              ref={path}
               d={`M 0 100 L 50 100 L 100 100 Z `}
-              fill="#ffffff6e"
+              fill="#ffffffe5"
               stroke="none"
             />
           </svg>
