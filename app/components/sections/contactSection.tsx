5d5e8c9736cd61bc984f039643ddd863775fbff9
Harsh Sahare <harshsahare1212@gmail.com>
2025-05-31 20:31:53 +0530

    refactored : made the contact section responsive

--- app/components/sections/contactSection.tsx
+++ app/components/sections/contactSection.tsx
@@ -5,61 +5,61 @@ const ContactSection = ({ className }: { className?: string }) => {
   return (
     <div
       id="contact"
-      className={`${className} flex contact-container min-h-screen `}
+      className={`${className} flex flex-col md:flex-row contact-container min-h-screen`}
     >
       {/* Left Section */}
-      <div className="w-1/2 bg-cover bg-center text-white p-10 flex flex-col justify-center relative">
-        <div className="w-full h-full absolute top-0 left-0 z-0">
+      <div className="relative w-full md:w-1/2 bg-cover bg-center text-white p-6 md:p-10 flex flex-col justify-center">
+        <div className="absolute inset-0 z-0">
           <Image
             src={"/images/contactUsBg.jpg"}
             alt="Background"
             fill
             className="object-cover"
           />
-          <div className="overlay absolute h-full w-full bg-[#00000060]"></div>
+          <div className="overlay absolute inset-0 bg-[#00000060]"></div>
         </div>
-        <div className="z-1 left">
-          <div>
-            <h1 className="text-5xl font-classica font-[700] leading-tight">
-              EXPRESS
-              <br />
-              YOUR
-              <br />
-              INTEREST
-            </h1>
-          </div>
-          <div className="mt-[200px]">
-            <p className="text-lg font-classica font-[400] mt-10">
+        <div className="relative z-10">
+          <h1 className="text-4xl sm:text-5xl font-classica font-[700] leading-tight">
+            EXPRESS
+            <br />
+            YOUR
+            <br />
+            INTEREST
+          </h1>
+
+          <div className="mt-20 sm:mt-40">
+            <p className="text-base sm:text-lg font-classica font-[400] mt-6">
               DISCOVER OUR PROJECT
             </p>
-            <p className="text-lg font-classica font-[400] mt-2">
+            <p className="text-base sm:text-lg font-classica font-[400] mt-2">
               FIND A SALES BOUTIQUES
             </p>
-            <div className="flex gap-4 item-center mt-8 text-2xl">
+
+            <div className="flex gap-4 items-center mt-6 sm:mt-8 text-2xl">
               <Image
                 src={"/images/facebook-white-outline.png"}
-                className="cursor-pointer w-[41px] h-[41px]"
+                className="cursor-pointer w-10 h-10"
                 alt="facebook"
                 width={41}
                 height={41}
               />
               <Image
                 src={"/images/instagram-white-outline.png"}
-                className="cursor-pointer w-[41px] h-[41px]"
+                className="cursor-pointer w-10 h-10"
                 alt="Instagram"
                 width={41}
                 height={41}
               />
               <Image
                 src={"/images/thread-white-outline.png"}
-                className="cursor-pointer w-[41px] h-[41px]"
+                className="cursor-pointer w-10 h-10"
                 alt="threads"
                 width={41}
                 height={41}
               />
               <Image
                 src={"/images/linkedin-white-outline.png"}
-                className="cursor-pointer w-[41px] h-[41px]"
+                className="cursor-pointer w-10 h-10"
                 alt="linkedin"
                 width={41}
                 height={41}
@@ -70,10 +70,10 @@ const ContactSection = ({ className }: { className?: string }) => {
       </div>
 
       {/* Right Section */}
-      <div className="w-1/2 p-18 bg-white  flex flex-col justify-center">
-        <form className="space-y-8 right">
+      <div className="w-full md:w-1/2 p-6 sm:p-12 bg-white flex flex-col justify-center">
+        <form className="space-y-8 px-10">
           <div>
-            <label className="block text-lg font-[400] font-classica">
+            <label className="block text-base sm:text-lg font-[400] font-classica">
               NAME
             </label>
             <input
@@ -83,7 +83,7 @@ const ContactSection = ({ className }: { className?: string }) => {
           </div>
 
           <div>
-            <label className="block text-lg font-[400] font-classica">
+            <label className="block text-base sm:text-lg font-[400] font-classica">
               MOBILE
             </label>
             <div className="flex items-center border-b border-black py-2">
@@ -93,7 +93,7 @@ const ContactSection = ({ className }: { className?: string }) => {
           </div>
 
           <div>
-            <label className="block text-lg font-[400] font-classica">
+            <label className="block text-base sm:text-lg font-[400] font-classica">
               EMAIL
             </label>
             <input
@@ -102,7 +102,7 @@ const ContactSection = ({ className }: { className?: string }) => {
             />
           </div>
 
-          <div className="space-y-4 text-sm font-classica font-[400] mt-[100px]">
+          <div className="space-y-4 text-sm font-classica font-[400] mt-12">
             <label className="flex items-center gap-2">
               <input type="checkbox" />
               TERM AND CONDITION
