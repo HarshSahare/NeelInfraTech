4d0f8193682805281742fc26a18c372c08ffb932
Harsh Sahare <harshsahare1212@gmail.com>
2025-06-04 17:25:41 +0530

    refacoted : added side navbar menu in the mobile view

--- app/components/layout/navbar.tsx
+++ app/components/layout/navbar.tsx
@@ -1,14 +1,41 @@
 "use client";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 import Image from "next/image";
 import Link from "next/link";
 import React, { useEffect, useState, useRef } from "react";
 
 function Navbar() {
   const prevY = useRef(0);
+  const mobile_menu = useRef<HTMLDivElement>(null);
+  const mobile_menu_container = useRef<HTMLDivElement>(null);
   const navbar = useRef<HTMLDivElement>(null);
   const [loaded, setLoaded] = useState(false);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const timeline = gsap.timeline();
   const isMenuOpenRef = useRef(isMenuOpen);
+
+  useGSAP(() => {
+    if (isMenuOpen) {
+      timeline.to(mobile_menu_container.current, {
+        opacity: 1,
+        duration: 0.3,
+      });
+      timeline.to(mobile_menu.current, {
+        translateX: "0%",
+        duration: 0.3,
+      });
+    } else {
+      timeline.to(mobile_menu.current, {
+        translateX: "100%",
+        duration: 0.3,
+      });
+      timeline.to(mobile_menu_container.current, {
+        opacity: 0,
+        duration: 0.3,
+      });
+    }
+  }, [isMenuOpen]);
 
   useEffect(() => {
     isMenuOpenRef.current = isMenuOpen;
@@ -68,48 +95,29 @@ function Navbar() {
   }, [isMenuOpen]);
 
   return (
-    <div
-      ref={navbar}
-      className={`fixed top-0 left-0 z-50 w-full h-[94px] backdrop-blur-md ${
-        isMenuOpen ? "bg-black/80" : "bg-[#00000042]"
-      } opacity-0 fadeIn delay-7s`}
-    >
-      <div className="w-[90%] mx-auto h-full items-center grid grid-cols-3">
-        {/* Desktop Left */}
-        <div className="hidden md:flex gap-10 font-classica text-[#bab4bba8]">
-          <Link className="hover:text-white cursor-pointer" href="/stories">
-            Our Story
-          </Link>
-          <Link className="hover:text-white cursor-pointer" href="/properties">
-            Our Properties
-          </Link>
-        </div>
+    <>
+      <div
+        ref={navbar}
+        className={`fixed top-0 left-0 z-50 w-full h-[94px] backdrop-blur-md ${
+          isMenuOpen ? "bg-black/80" : "bg-[#00000042]"
+        } opacity-0 fadeIn delay-7s`}
+      >
+        <div className="w-[90%] mx-auto h-full items-center grid grid-cols-3">
+          {/* Desktop Left */}
+          <div className="hidden md:flex gap-10 font-classica text-[#bab4bba8]">
+            <Link className="hover:text-white cursor-pointer" href="/stories">
+              Our Story
+            </Link>
+            <Link
+              className="hover:text-white cursor-pointer"
+              href="/properties"
+            >
+              Our Properties
+            </Link>
+          </div>
 
-        {/* Desktop Logo */}
-        <div className="hidden md:flex justify-center">
-          <Image
-            className="h-auto"
-            src="/images/logo.png"
-            alt="logo"
-            width={160}
-            height={32}
-            quality={80}
-          />
-        </div>
-
-        {/* Desktop Right */}
-        <div className="hidden md:flex justify-end">
-          <a
-            href="#contact"
-            className="ring ring-black text-black py-2 px-5 hover:bg-black hover:text-white cursor-pointer"
-          >
-            Contact Us
-          </a>
-        </div>
-
-        {/* Mobile Layout */}
-        <div className="flex items-center justify-between w-full md:hidden col-span-3 px-2">
-          <div>
+          {/* Desktop Logo */}
+          <Link className="hidden md:flex justify-center" href={"/"}>
             <Image
               className="h-auto"
               src="/images/logo.png"
@@ -118,31 +126,73 @@ function Navbar() {
               height={32}
               quality={80}
             />
+          </Link>
+
+          {/* Desktop Right */}
+          <div className="hidden md:flex justify-end">
+            <a
+              href="#contact"
+              className="ring ring-black text-black py-2 px-5 hover:bg-black hover:text-white cursor-pointer"
+            >
+              Contact Us
+            </a>
           </div>
-          <button
-            className="text-white text-3xl"
-            onClick={() => setIsMenuOpen((prev) => !prev)}
-          >
-            ☰
-          </button>
+
+          {/* Mobile Layout */}
+          <div className="flex items-center justify-between w-full md:hidden col-span-3 px-2">
+            <div>
+              <Image
+                className="h-auto"
+                src="/images/logo.png"
+                alt="logo"
+                width={160}
+                height={32}
+                quality={80}
+              />
+            </div>
+            <button
+              className="text-white text-3xl cursor-pointer"
+              onClick={() => setIsMenuOpen((prev) => !prev)}
+            >
+              ☰
+            </button>
+          </div>
         </div>
+
+        {/* Mobile Dropdown */}
       </div>
 
-      {/* Mobile Dropdown */}
-      {isMenuOpen && (
-        <div className="md:hidden px-6 pt-4 pb-6 bg-black/80 text-white text-lg flex flex-col gap-4 font-classica backdrop-blur-md">
-          <Link href="/stories" onClick={() => setIsMenuOpen(false)}>
-            Our Story
-          </Link>
-          <Link href="/properties" onClick={() => setIsMenuOpen(false)}>
-            Our Properties
-          </Link>
-          <a href="#contact" onClick={() => setIsMenuOpen(false)}>
-            Contact Us
-          </a>
-        </div>
-      )}
-    </div>
+      <div
+        ref={mobile_menu_container}
+        className={`fixed flex w-screen h-screen bottom-0 bg-[#0000007c] z-100 opacity-0 ${
+          isMenuOpen ? "pointer-events-auto" : "pointer-events-none"
+        }`}
+        onClick={() => setIsMenuOpen(false)}
+      ></div>
+      <div
+        ref={mobile_menu}
+        className="fixed z-101 right-0 translate-x-[100%] md:hidden w-[60%] h-screen px-6 pt-4 pb-6 bg-black/80 text-white text-lg flex flex-col gap-4 font-classica backdrop-blur-md"
+        onClick={(e) => {
+          e.stopPropagation();
+        }}
+      >
+        <button
+          className="text-5xl self-end cursor-pointer"
+          onClick={() => setIsMenuOpen(false)}
+        >
+          &times;
+        </button>
+        <Link href="/stories" onClick={() => setIsMenuOpen(false)}>
+          Our Story
+        </Link>
+        <Link href="/properties" onClick={() => setIsMenuOpen(false)}>
+          Our Properties
+        </Link>
+        <a href="#contact" onClick={() => setIsMenuOpen(false)}>
+          Contact Us
+        </a>
+      </div>
+    </>
   );
 }
 
