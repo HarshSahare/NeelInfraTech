2a709222180d247959d550a92ea070d394767367
Harsh Sahare <harshsahare1212@gmail.com>
2025-06-07 11:57:55 +0530

    refactored : Added directors images

--- app/components/sections/directorsSection.tsx
+++ app/components/sections/directorsSection.tsx
@@ -1,171 +1,33 @@
-// "use client";
-// import Image from "next/image";
-// import React, { useEffect, useRef, useState } from "react";
-// import gsap from "gsap";
-// import { useGSAP } from "@gsap/react";
-
-// function DirectorsSection() {
-//   const value = useRef(1);
-//   const [currentIndex, setCurrentIndex] = useState(2);
-//   const images = [
-//     "/images/director-image.png",
-//     "/images/director-image.png",
-//     "/images/director-image.png",
-//     "/images/director-image.png",
-//     "/images/director-image.png",
-//   ];
-
-//   useEffect(() => {
-//     const interval = setInterval(() => {
-//       setCurrentIndex((prevIndex) => {
-//         if (prevIndex === images.length - 1) {
-//           value.current = -1;
-//         }
-//         if (prevIndex === 0) {
-//           value.current = 1;
-//         }
-//         return prevIndex + value.current;
-//       });
-//     }, 1000);
-
-//     return () => {
-//       clearInterval(interval);
-//     };
-//   }, []);
-
-//   useGSAP(() => {
-//     gsap.to(".director-1-image", {
-//       // x: `${currentIndex != 0 ? (2 - currentIndex) * 100 : 100}%`,
-//       opacity: `${currentIndex != 0 ? 0.5 : 1}`,
-//       width: `${currentIndex != 0 && "100%"}`,
-//       duration: 0.5,
-//     });
-//     gsap.to(".director-2-image", {
-//       // x: `${
-//       //   currentIndex != 1
-//       //     ? currentIndex == 0
-//       //       ? 200
-//       //       : currentIndex == 2
-//       //       ? 0
-//       //       : currentIndex == 3
-//       //       ? -100
-//       //       : currentIndex == 4 && -200
-//       //     : 50
-//       // }%`,
-//       opacity: `${currentIndex != 1 ? 0.5 : 1}`,
-//       width: `${currentIndex != 1 ? "80%" : "100%"}`,
-//       duration: 0.5,
-//     });
-//     gsap.to(".director-3-image", {
-//       // x: `${
-//       //   currentIndex != 2
-//       //     ? currentIndex == 0
-//       //       ? 200
-//       //       : currentIndex == 1
-//       //       ? 100
-//       //       : currentIndex == 3
-//       //       ? -100
-//       //       : currentIndex == 4 && -200
-//       //     : 0
-//       // }%`,
-//       opacity: `${currentIndex != 2 ? 0.5 : 1}`,
-
-//       duration: 0.5,
-//     });
-//     gsap.to(".director-4-image", {
-//       // x: `${
-//       //   currentIndex != 3
-//       //     ? currentIndex == 0
-//       //       ? 200
-//       //       : currentIndex == 2
-//       //       ? 0
-//       //       : currentIndex == 1
-//       //       ? 100
-//       //       : currentIndex == 4 && -200
-//       //     : -50
-//       // }%`,
-//       opacity: `${currentIndex != 3 ? 0.5 : 1}`,
-//       width: `${currentIndex != 3 ? "80%" : "100%"}`,
-//       duration: 0.5,
-//     });
-//     gsap.to(".director-5-image", {
-//       // x: `${currentIndex != 4 ? (2 - currentIndex) * 100 : -100}%`,
-//       opacity: `${currentIndex != 4 ? 0.5 : 1}`,
-//       width: `${currentIndex != 4 ? "80%" : "100%"}`,
-//       duration: 0.5,
-//     });
-//   }, [currentIndex]);
-
-//   return (
-//     <div className="min-h-svh overflow-x-hidden vision-container w-[95.5%] pt-30 m-auto flex flex-col">
-//       <div>
-//         <div className="mx-auto w-fit">
-//           <div className="uppercase w-fit font-classica heading text-[75px] font-[400] text-center mt-5">
-//             Mr. Ajay Dandekar
-//           </div>
-//           <div className="w-fit text-[#44404096] heading font-poppins">
-//             DIRECTORS Of NEEL INFRATECH
-//           </div>
-//         </div>
-//       </div>
-
-//       <div className="flex justify-end">
-//         <div className="w-fit font-classica text-[12px]/[24px] text-[#918F8F] font-[400] pt-10 ">
-//           <div className="mt-2 ml-[-5px]">
-//             Starting with nothing but a dream and determination, Mr. Pitambar
-//             Ikar <br /> built Neel Infratech into a trusted empire through
-//             vision, hard work, and <br /> unwavering belief. belief.
-//             <div className="h-[0.2px] w-[40%] my-3 bg-[#918F8F]"></div>
-//             Nov 21 2017
-//           </div>
-//         </div>
-//       </div>
-
-//       <div className="grid mt-[-10%] grid-cols-6 items-end justify-items-center w-full m-auto max-w-[80%]">
-//         {images.map((image, index) => (
-//           <Image
-//             src={image}
-//             key={index}
-//             alt={`Director ${index + 1}`}
-//             className={` h-auto w-[80%]
-//             } director-${index + 1}-image  ${
-//               currentIndex == index ? " col-span-2" : "col-span-1"
-//             }`}
-//             width={1024}
-//             height={700}
-//           />
-//         ))}
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default DirectorsSection;
-
 "use client";
 import { useRef } from "react";
 import Image from "next/image";
-
-const directors = [
-  {
-    name: "Zaha Hadid",
-    image: "/images/director-image.png",
-  },
-  {
-    name: "Foster + Partners",
-    image: "/images/storiesImage12.png",
-  },
-  {
-    name: "Gilles & Boissier",
-    image: "/images/director-image.png",
-  },
-  {
-    name: "Jean Nouvel",
-    image: "/images/director-image.png",
-  },
-];
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { DirectorsInfo } from "@/app/lib/directors";
 
 export default function DirectorsSection() {
+  useGSAP(() => {
+    const containers = document.querySelectorAll(".director-section > div");
+    gsap.from(containers[0], {
+      y: 50,
+      duration: 1,
+      opacity: 0,
+      scrollTrigger: {
+        trigger: containers[0],
+        start: "top bottom-=20%",
+      },
+    });
+    gsap.from(containers[1], {
+      y: 50,
+      duration: 1,
+      opacity: 0,
+      scrollTrigger: {
+        trigger: containers[0],
+        start: "top bottom-=20%",
+      },
+    });
+  }, []);
+
   const scrollContainerRef = useRef<HTMLDivElement | null>(null);
   const cardRefs = useRef<(HTMLDivElement | null)[]>([]);
 
@@ -203,33 +65,34 @@ export default function DirectorsSection() {
   };
 
   return (
-    <section className="bg-white px-4 md:px-10">
+    <section className="bg-white px-4 md:px-10 director-section">
+      <div className="text-left mb-8 px-4">
+        <h2 className="text-[32px] sm:text-[40px] md:text-[45px] lg:text-5xl heading mt-5 text-[#161616] font-classica font-[400] leading-tight">
+          Directors
+        </h2>
+      </div>
       <div
         ref={scrollContainerRef}
         className="flex gap-6 overflow-x-auto scroll-smooth scrollbar-hide py-6"
       >
-        {directors.map((designer, index) => (
+        {DirectorsInfo.map((data, index) => (
           <div
             key={index}
             ref={(el) => {
               cardRefs.current[index] = el;
             }}
-            className="flex-shrink-0 border border-gray-200 shadow-md rounded-md"
+            className="flex-shrink-0 flex flex-col border border-gray-200 shadow-md rounded-md"
           >
             <Image
-              src={designer.image}
-              alt={designer.name}
+              src={data.image}
+              alt={data.name}
               width={index === 1 ? 500 : 400} // different widths
               height={350}
-              className="object-cover h-[350px] w-auto rounded-t-md"
+              className="object-contain h-[350px] w-auto rounded-t-md"
             />
             <div className="p-4 text-center">
-              <p className="text-lg font-semibold text-gray-800">
-                {designer.name}
-              </p>
-              <p className="text-sm italic text-gray-500 mt-1">
-                “Visionary design beyond boundaries.”
-              </p>
+              <p className="text-lg font-semibold text-gray-800">{data.name}</p>
+              <p className="text-sm italic text-gray-500 mt-1">{data.quote}</p>
             </div>
           </div>
         ))}
--- app/stories/page.tsx
+++ app/stories/page.tsx
@@ -1,98 +1,9 @@
 import Image from "next/image";
 import React from "react";
+import { DirectorsInfo } from "../lib/directors";
 
-interface DirectorsInfoInterface {
-  name: string;
-  quote: string;
-  description: string;
-  image: string;
-  subDescription: string;
-  quoteAuthor: string;
-}
 
 function StoriesPage() {
-  const DirectorsInfo: DirectorsInfoInterface[] = [
-    {
-      name: "Mr. Ajay Dandekar",
-      quote: `"Rasta chalna, saaf chalna – chahe usme darr lage."`,
-      description: `
-      Ajay Dandekar’s path from debt to direction began in 1988, after completing his BSW and MSW. During his M.A., he entered network marketing, rose to Silver Star, but landed in ₹3.5 lakhs debt—realizing success lies in selling value, not just dreams.
-      <br/><br/>An introvert turned communicator, Ajay now holds over 8,000 contacts in his phone. Despite early setbacks and ₹4.5 lakhs debt post-marriage, he co-founded Neel Infratech with ex-marketing peers—becoming the first to join.
-      <br/><br/>Real estate changed everything. He paid off his debts, bought five properties, helped 400+ people build businesses, and fulfilled his dream of a family foreign trip in 2021. His only regret: his parents didn’t see this success.
-`,
-      image: "/images/storiesImage3.png",
-      subDescription:
-        '"Dandekar Sir and Neel Infratech didn’t just sell us a plot — they educated us. The site visit gave us full clarity, and we were convinced to invest. Today, we’re seeing great returns thanks to the location.”',
-      quoteAuthor: "Mr. Praful Gourkar",
-    },
-    {
-      name: "Mr. Onkar Sonwani Sir",
-      quote: `""Irade nek ho aur mehnat sacchi ho toh har sapna haqiqat ban sakta hai."`,
-      description: `
-      Onkar Sir began his journey of hard work and ambition at the young age of 20. Starting with network marketing, he moved into health product-based marketing, where he gained valuable experience in sales, communication, and team building.
-      <br/><br/>Through his network, he met Ikar Sir, which led him to enter the world of real estate. That decision changed his life. With determination and the support of Neel Infratech, he turned dreams into reality—buying a house and multiple plots in Nagpur, securing a stable and successful future for his family.
-      `,
-      image: "/images/storiesImage3.png",
-      subDescription:
-        "“Onkar Sir guided us throughout the entire process, making it easy and trustworthy. Thanks to his support, we confidently invested with Neel Infratech. The location, planning, and transparency were impressive — and we’re already seeing growth in our investment.”",
-      quoteAuthor: "Mr. Wakodekar",
-    },
-    {
-      name: "Mr. Satendra Chaudhari Sir",
-      quote:
-        '"Karam pe vishvash rakho, result pe nahi — result aapka karam hi dilayega."',
-      description: `
-      Hailing from Manjhuriya village in Darbhanga district, Satendra Sir's life journey reflects resilience and responsibility. Married at a young age, he moved to Nagpur at 25 with his wife and three children, determined to give them a better future through education.
-      <br/><br/>To support his family, he took on various jobs over the years—never stepping back from hard work. With the support of a friend, he entered the real estate industry, where his dedication and sincerity led him to co-build a company and eventually become its Director.
-      <br/><br/>Through his real estate earnings, he educated his children, arranged their marriages, and secured their futures by buying properties and land. By the age of 45, he had firmly established himself. Today, he stands proud with 700–800 happy clients across Nagpur, having sold property in both private and government sectors—with customer satisfaction at the core of his journey.
-        `,
-      image: "/images/storiesImage3.png",
-      subDescription:
-        '"What Chaudhari Sir promises is exactly what he delivers. I purchased property from Neel Infratech through him, and I’m very satisfied. The property is excellent, and their support with technical documentation was smooth and reliable — we didn’t have to worry about a thing. I’m truly happy with my decision to buy from Chaudhari Sir."',
-      quoteAuthor: "Mr. Gagaram Sakharkar",
-    },
-    {
-      name: "Mr. Dekate Sir",
-      quote: `"Jab tak thakte nahi, tab tak haarte nahi."`,
-      description: `
-      Born in Kargaon village, Nagpur, Dekate Sir came from a humble farming background. After completing his M.A. in English Literature from Morris College along with B.Ed. and BML, he began working as a Junior College Lecturer in 1995.
-      <br/><br/>Despite a stable job, he pursued business—trying dairy, nursery, and MLM—which led to a ₹35 lakh debt. In 2009, real estate became his turning point. Joining Neel Infratech at 42, he cleared his debt and achieved financial freedom by 45.
-      <br/><br/>Today, he owns homes in Nagpur and his village, farmland, and commercial/residential plots, proving it's never too late to succeed.
-    `,
-      image: "/images/storiesImage3.png",
-
-      subDescription:
-        "“I bought a property from Dekate Sir and I’m very happy with my purchase. The company is trustworthy, the owner is supportive, and all the documentation was done properly. It was a smooth and satisfying experience.”",
-      quoteAuthor: "Mr. Vilas Kature",
-    },
-    {
-      name: "Mr. Rupesh Sir",
-      quote: `"Aadmi zindagi mein utna hi bada ban sakta hai, jitni badi uski soch ho."`,
-      description: `
-      Born into a humble farming family, Rupesh Sir has always believed in the power of vision and effort. While pursuing his B.A., he stepped into the world of business and soon met Ikar Sir, the current owner of Neel Infratech. That meeting marked the beginning of his journey in the real estate industry.
-      <br/><br/>Over the past 11 years at Neel Infratech, he has not only contributed to the company’s success but also achieved several personal milestones—buying his first car, supporting his daughter’s education, purchasing his dream car, and successfully closing multiple property deals across Nagpur.
-      `,
-      image: "/images/storiesImage3.png",
-      subDescription:
-        "“I bought a property from Rupesh Sir and Neel Infratech, and I’m very happy with the experience. The process was smooth, the title was clear, and I’m glad I got a plot on Wardha Road — the best area to invest today.”",
-      quoteAuthor: "Mr. Rajesh Bandale",
-    },
-    {
-      name: "Mr. Pushparaj Meshram",
-      quote:
-        '"Hume zarurat ke hisaab se nahi, chahat ke hisaab se jeena chahiye."',
-      description: `
-      Born near Bhivapoor village, Umred, Pushparaj Sir started working in 7th grade to support his education. After moving to Nagpur, he completed his LLB and LLM—taking up jobs like digging, book selling, and tutoring to fund his journey.
-      <br/><br/>He began practicing law in 2000, but in 2007, real estate sparked a new direction. By 2009, he committed fully to the field. With 18+ years of experience and 10 years at Neel Infratech, he’s served over 2,000 clients and mentored 500+ budding realtors.
-      <br/><br/>From rural roots to international trips like Andaman and Thailand, his journey proves that dedication changes destiny.
-      `,
-      image: "/images/storiesImage3.png",
-      subDescription:
-        '"Pushparaj Sir made buying my property easy and clear. He helped me find a great home with no hassle. Very honest and professional"',
-      quoteAuthor: "Mr. Rajesh K. Homebuyer",
-    },
-  ];
-
   return (
     <div className="flex flex-col">
       <div className="h-svh w-screen bg-amber-50">
@@ -396,3 +307,4 @@ function StoriesPage() {
 }
 
 export default StoriesPage;
+
