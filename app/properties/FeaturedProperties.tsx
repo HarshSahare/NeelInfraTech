afb58e74c3e53397d28b6ead64c04402d9ec3b8e
Harsh Sahare <harshsahare1212@gmail.com>
2025-08-11 16:17:17 +0530

    refactored : added image in the featured property silver star

--- app/properties/FeaturedProperties.tsx
+++ app/properties/FeaturedProperties.tsx
@@ -25,25 +25,25 @@ export default function FeaturedProperties({
       </h2>
       <div className="flex flex-col lg:flex-row gap-10 w-[80%] mx-auto py-15">
         {properties.map((card, i) => (
-          <div key={i} className="flex-1">
+          <div className="flex-1" key={i}>
             <div className="relative">
               <Image
                 src={card.src}
-                alt={card.title}
+                alt="property image"
                 width={1200}
                 height={700}
-                className="w-full aspect-video rounded-2xl"
+                className="w-full aspect-video z-1 rounded-2xl"
               />
-              <div className="absolute bottom-0 left-0 bg-black/50 text-white p-4 text-3xl uppercase font-semibold">
+              <div className="font-classica absolute bottom-0 left-0 text-white z-2  px-8 py-5 text-3xl uppercase font-[500]">
                 {card.title}
               </div>
             </div>
-            <div className="mt-5 text-[#757279]">
-              <div className="flex items-center gap-2 text-xl">
-                <FaLocationDot />
+            <div className=" py-5  text-[#757279] ">
+              <div className="flex  items-center gap-2 text-xl">
+                <FaLocationDot className="text-xl" />
                 {card.location}
               </div>
-              <p className="mt-2">{card.desc}</p>
+              <div className="text-md py-2">{card.desc}</div>
             </div>
           </div>
         ))}
